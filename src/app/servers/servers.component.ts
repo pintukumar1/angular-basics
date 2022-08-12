7ea2eaada14e5d1e64d408dabf622fdd94846f70
Pintu Kumar <kumarpintu1713122@gmail.com>
2022-08-12 09:37:43 +0530

    directives and basics

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgModel } from '@angular/forms';
 @Component({
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server><app-server></app-server>',
+  // template: '<app-server></app-server><app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false
+  serverCreationStatus = "No server was created !!"
+  serverName = 'TestServer'
 
-  constructor() { }
+  constructor() { 
+     setTimeout(() => {
+        this.allowNewServer = true
+     }, 2000)
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created, server name is' + this.serverName
+  }
+
+  onUpdateServerName(event){
+    console.log(event.target.value)
+    this.serverName = event.target.value
+  }
 
   ngOnInit(): void {
   }
